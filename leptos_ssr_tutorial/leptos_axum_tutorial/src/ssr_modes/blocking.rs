2c4caff6b3a61a8ab919ed2dada089f0d1bb576c
Oliver Cunnington <ofcunnington@hotmail.com>
2026-05-30 13:08:08 +0200

    leptos ssr

---
+++ leptos_ssr_tutorial/leptos_axum_tutorial/src/ssr_modes/blocking.rs
@@ -0,0 +1,25 @@
+#[component]
+pub fn BlogPost() -> impl IntoView {
+    let post_data = Resource::new_blocking(/* load blog post */);
+    let comments_data = Resource::new(/* load blog comments */);
+    view! {
+        <Suspense fallback=|| ()>
+            {move || Suspend::new(async move {
+                let data = post_data.await;
+                view! {
+                    <Title text=data.title/>
+                    <Meta name="description" content=data.excerpt/>
+                    <article>
+                        /* render the post content */
+                    </article>
+                }
+            })}
+        </Suspense>
+        <Suspense fallback=|| "Loading comments...">
+            {move || Suspend::new(async move {
+                let comments = comments_data.await;
+                todo!()
+            })}
+        </Suspense>
+    }
+}
---
+++ leptos_ssr_tutorial/leptos_axum_tutorial/src/ssr_modes/out_of_order.rs
@@ -0,0 +1,12 @@
+// <Routes fallback=|| "Not found.">
+//     // We’ll load the home page with out-of-order streaming and <Suspense/>
+//     <Route path=path!("") view=HomePage/>
+
+//     // We'll load the posts with async rendering, so they can set
+//     // the title and metadata *after* loading the data
+//     <Route
+//         path=path!("/post/:id")
+//         view=BlogPost
+//         ssr=SsrMode::Async
+//     />
+// </Routes>
