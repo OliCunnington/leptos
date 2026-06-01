61250deec730a3c2b3df2bafac4953e18267b1b1
Oliver Cunnington <ofcunnington@hotmail.com>
2026-06-01 09:14:52 +0200

    leptos ssr

--- leptos_ssr_tutorial/leptos_axum_tutorial/src/ssr_modes/blog_elements/blog_posts.rs
+++ leptos_ssr_tutorial/leptos_axum_tutorial/src/ssr_modes/blog_elements/blog_posts.rs
@@ -1,17 +1,44 @@
 use leptos::prelude::*;
 
-#[component]
-pub fn BlogPost() -> impl IntoView {
-    view!{
+pub struct Post {
+    post: PostContent,
+    comments: Vec<Comment>
+}
 
+#[derive(Deserialize, Serialize)]
+pub struct PostContent {
+    user: String,
+    postData: String
+}
+
+#[derive(Deserialize, Serialize)]
+pub struct Comment {
+    user: String,
+    comment: String
+}
+
+#[component]
+pub fn BlogPost(post : PostContent) -> impl IntoView {
+    view!{
+        <div class="post">
+            <p>{post.user}</p>
+            <p>{post.postData}</p>
+        </div>
     }
 }
 
 
 #[component]
-pub fn BlogPostComment() -> impl IntoView {
+pub fn BlogPostComment(comment : Comment) -> impl IntoView {
     view!{
-        
+        <div class="comment">
+            <p>{comment.user}</p>
+            <p>{comment.comment}</p>
+        </div>   
     }
 }
 
+// i only need the resource content ???
+    // let post_data = Resource::new_blocking(/* load blog post */);
+    // let comments_data = Resource::new(/* load blog comments */);
+// deez...
