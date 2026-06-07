5ed896695bfb8274bb550488e118238619b06c9d
Oliver Cunnington <ofcunnington@hotmail.com>
2026-06-07 12:38:58 +0200

    leptos ssr

--- leptos_ssr_tutorial/leptos_axum_tutorial/src/ssr_modes/synchronous.rs
+++ leptos_ssr_tutorial/leptos_axum_tutorial/src/ssr_modes/synchronous.rs
@@ -4,6 +4,9 @@ use leptos::prelude::*;
 pub fn SynchronousRendering() {
     view!{
         <div>
+            <Suspense fallback=|| (view!{<p>"Loading..."</p>})>
+                
+            </Suspense>
         </div>
     }
 }
