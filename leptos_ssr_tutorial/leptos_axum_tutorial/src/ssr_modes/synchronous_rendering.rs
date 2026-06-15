ca09723033b6922c3ef3f874a573ff4cd88252a9
Oliver Cunnington <ofcunnington@hotmail.com>
2026-06-15 12:42:58 +0200

    leptos ssr

--- leptos_ssr_tutorial/leptos_axum_tutorial/src/ssr_modes/synchronous_rendering.rs
+++ leptos_ssr_tutorial/leptos_axum_tutorial/src/ssr_modes/synchronous_rendering.rs
@@ -2,6 +2,22 @@ use leptos::prelude::*;
 
 #[component]
 pub fn SynchronousRendering() {
+
+    let (id, set_id) = create_signal
+    // let comments = LocalResource::new()
+    let comments = create_local_resource(
+        //scope
+        cx,
+        //source (signal)
+        move || id.get(),
+        //fetcher
+        move |id| {
+            let id = id;
+            async move {
+                blog_elements::blog_posts::get_post(id).await
+            }
+        }
+    );
     view!{
         <div>
             <Suspense fallback=|| (view!{<p>"Loading..."</p>})>
