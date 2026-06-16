a917bef887b2d15fcf05c44bcecc0995f7681470
Oliver Cunnington <ofcunnington@hotmail.com>
2026-06-16 11:54:02 +0200

    leptos ssr

--- leptos_ssr_tutorial/leptos_axum_tutorial/src/ssr_modes/synchronous_rendering.rs
+++ leptos_ssr_tutorial/leptos_axum_tutorial/src/ssr_modes/synchronous_rendering.rs
@@ -3,25 +3,44 @@ use leptos::prelude::*;
 #[component]
 pub fn SynchronousRendering() {
 
-    let (id, set_id) = create_signal
-    // let comments = LocalResource::new()
-    let comments = create_local_resource(
-        //scope
-        cx,
-        //source (signal)
-        move || id.get(),
-        //fetcher
-        move |id| {
-            let id = id;
-            async move {
-                blog_elements::blog_posts::get_post(id).await
-            }
+    let comments = LocalResource::new(
+        async move {
+            blog_elements::blog_posts::get_comments().await
         }
     );
+    
+    let posts = LocalResource::new(
+        async move {
+            blog_elements::blog_posts::get_posts().await
+        }
+    );
+
+
+    // let (id, set_id) = create_signal
+    // let comments = create_local_resource(
+    //     //scope
+    //     cx,
+    //     //source (signal)
+    //     move || id.get(),
+    //     //fetcher
+    //     move |id| {
+    //         let id = id;
+    //         async move {
+    //             blog_elements::blog_posts::get_post(id).await
+    //         }
+    //     }
+    // );
+
+
     view!{
         <div>
+            <h2>"Posts"</h2>
             <Suspense fallback=|| (view!{<p>"Loading..."</p>})>
-                // local resources? eh?
+                
+            </Suspense>
+            <h2>"Comments"</h2>
+            <Suspense fallback=|| (view!{<p>"Loading..."</p>})>
+
             </Suspense>
         </div>
     }
