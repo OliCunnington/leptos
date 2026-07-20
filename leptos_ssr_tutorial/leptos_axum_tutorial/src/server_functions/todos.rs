993b711d02050ea3db33e9b393bd0a6eb1ff4ee0
Oliver Cunnington <ofcunnington@hotmail.com>
2026-07-20 13:07:31 +0200

    leptos ssr

--- leptos_ssr_tutorial/leptos_axum_tutorial/src/server_functions/todos.rs
+++ leptos_ssr_tutorial/leptos_axum_tutorial/src/server_functions/todos.rs
@@ -1,18 +1,7 @@
 use leptos::prelude::*;
+use serde::{Deserialize, Serialize};
+use leptos::task::spawn_local;
 
-#[server]
-pub async fn add_todo(title: String) -> Result<(), ServerFnError> {
-    let mut conn = db().await?;
-
-    match sqlx::query("INSERT INTO todos (title, completed) VALUES ($1, false)")
-        .bind(title)
-        .execute(&mut conn)
-        .await
-    {
-        Ok(_row) => Ok(()),
-        Err(e) => Err(ServerFnError::ServerError(e.to_string())),
-    }
-}
 
 #[component]
 pub fn BusyButton() -> impl IntoView {
@@ -94,3 +83,4 @@ pub async fn add_todo(title: String) -> Result<(), ServerFnError> {
         Err(e) => Err(ServerFnError::ServerError(e.to_string())),
     }
 }
+
